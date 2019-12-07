e7eb5417549dc23089a33475070a26784b070fe2
Agnes Jang <agnesj@uci.edu>
2019-12-07 15:26:32 -0800

    recover polyline branch

--- mobileapp/ios/Runner/AppDelegate.swift
+++ mobileapp/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBPjg4jWeywzEZZPdRyz8RGoSj6W3qC6gI")
+    GMSServices.provideAPIKey("YOUR_API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
